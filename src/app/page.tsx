0fc02cae515bfbe61086c17d0820fc3e8786b5bd
scarletvargas <scarletvf47@gmail.com>
2024-01-31 22:39:44 -0400

    changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,37 @@
+"use client";
+import axios from "axios";
+import { format, parseISO } from "date-fns";
+import { useQuery } from "react-query";
+
 export default function Home() {
-  return <div>Hello</div>;
+  const { isLoading, error, data } = useQuery<WeatherData>(
+    "repoData",
+    async () => {
+      const { data } = await axios.get(
+        `https://api.openweathermap.org/data/2.5/forecast?q=pune&appid=${process.env.NEXT_PUBLIC_WEATHER_KEY}&cnt=15`
+      );
+      return data;
+    }
+  );
+
+  if (isLoading)
+    return (
+      <div className="flex items-center justify-center">
+        <p className="animate-bounce">Loading...</p>
+      </div>
+    );
+
+  return (
+    <main className="px-3 max-w-7xl mx-auto flex flex-col gap-9 w-full pb-10 pt-4">
+      <section>
+        <div>
+          <div className="flex gap-1 text-2xl items-end">
+            <p>({format(parseISO(firstData?.dt_txt ?? ""), "EEEE")})</p>
+            <p className="text-">({format(parseISO(firstData?.dt_txt ?? ""), "EEEE")})</p>
+          </div>
+        </div>
+      </section>
+      <section></section>
+    </main>
+  );
 }
