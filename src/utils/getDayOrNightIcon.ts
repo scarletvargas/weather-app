30e8b35d97d796e05c5761e4982c836759562194
scarletvargas <scarletvf47@gmail.com>
2024-03-25 23:16:55 -0400

    folder utils

---
+++ src/utils/getDayOrNightIcon.ts
@@ -0,0 +1,7 @@
+export function getDayOrNightIcon(iconName: string, dateTimeString: string): string {
+  const hours = new Date(dateTimeString).getHours(); // Get hours from the given date and time
+
+  const isDayTime = hours >= 6 && hours < 18; // Consider daytime from 6 AM to 6 PM
+
+  return isDayTime ? iconName.replace(/.$/, "d") : iconName.replace(/.$/, "n");
+}
---
+++ src/utils/index.ts
@@ -0,0 +1,5 @@
+export * from "./cn";
+export * from "./convertKelvinToCelsius";
+export * from "./convertWindSpeed";
+export * from "./getDayOrNightIcon";
+export * from "./metersToKilometers";
